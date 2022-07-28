8a56b08b9354d6012597073084e5531b535bc6b0
lchilumula <110132270+lchilumula@users.noreply.github.com>
2022-07-27 22:08:28 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html lang="en"> <head> 
+<meta charset="utf-8"> 
+<title> Intelligent search</title> </head> 
+<body>
+<form action="serp.php" method="post"> 
+<label> query input</label>
+<input type ="text" name="q" placeholder="Type your search query"> <button type="submit"> search </button> 
+</form> </body> 
+</html> 
