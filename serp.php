33a343e20e23ef248b87caab10c48ac602ebca33
lchilumula <110132270+lchilumula@users.noreply.github.com>
2022-07-27 22:09:40 -0400

    Create serp.php

---
+++ serp.php
@@ -0,0 +1,24 @@
+<?php
+// establishing a DB connection 
+static $dbconn; 
+if(!isset($dbconn)) {
+$config = parse_ini_file('.pm3_db_config.ini');
+$conn_str = "host={$config['servername']} user={$config['username']} 
+password={$config['password']} dbname={$config['dbname']}"; try{ 
+$dbconn= pg_connect($conn_str); } catch (Exception $e) { 
+echo $e->getMessage(); } 
+} 
+$q_phrase = pg_escape_string($_POST['q']);
+// Retriving up to 5 documents that contains the query phrase
+$sql = "SELECT * FROM article WHERE abstract ~ '$q_phrase' LIMIT 5;"; $result = pg_query($dbconn, $sql);
+?> 
+<html lang="en"> <head> 
+<title>Search Results</title> </head> 
+<body>
+<h2> article contain '<?php echo $q_phrase ?>' </h2> <ul> 
+<?php
+while ($row = pg_fetch_row($result)) { 
+echo "<li>{$row[1]}</li>"; } 
+?> 
+</ul> 
+</body> </html> 
